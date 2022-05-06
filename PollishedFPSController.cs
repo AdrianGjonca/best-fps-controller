9b96abb38dd6be1fca242e3937e889eb89cdec63
AdrianGjonca <55891807+AdrianGjonca@users.noreply.github.com>
2022-05-06 12:17:28 +0100

    Created it
    Making this at school so I cant really test this rn

---
+++ PollishedFPSController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PollishedFPSController : CharacterController
+{
+  void Start()
+  {
+
+  }
+  void Update()
+  {
+
+  }
+}
